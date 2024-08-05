c0018ffefc8c5942edb30ee1d0b420ed5bcfb5b0
Julio Vieira <juliostefano1327@gmail.com>
2024-08-05 19:55:44 -0300

    Testando Trigger com INSERT, UPDATE, DELETE

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -8,3 +8,17 @@ BEGIN
 	PRINT 'Os dados da tabela foram alterados'
 
 END
+
+
+SELECT * FROM dCliente
+
+INSERT INTO dCliente(nome_cliente,genero, data_de_nascimento, cpf) 
+VALUES
+	('Julio Stefano','M','16/04/2000', 11122233345)
+
+UPDATE dCliente
+SET cpf = '111.222.333-46'
+WHERE id_cliente = 11
+
+DELETE FROM dCliente
+WHERE id_cliente = 11
